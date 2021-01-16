bd8f0bf3f6e6217a8aac6a873d61072d5b929aae
Timofiy <ez.lvl.faceit@gmail.com>
2021-01-16 16:54:12 +0200

    Initial commit

---
+++ scripts/main.js
@@ -0,0 +1,12 @@
+window.onscroll = function showHeader() {
+    
+    var header = document.querySelector('.header');
+    
+    if (window.pageYOffset > 0) {
+        header.classList.add('header__fixed');
+    } else{
+       header.classList.remove('header__fixed'); 
+    }
+    
+    
+};
