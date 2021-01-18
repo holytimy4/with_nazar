657849cc69f3e125575bd71e3add00d3a25f6800
seclud3d <ez.lvl.faceit@gmail.com>
2021-01-18 10:48:51 +0200

    version 0.9
    Added footer /
    
    waiting to fix the anchors!
    
    Co-Authored-By: sankevitch <75832969+sankevitch@users.noreply.github.com>

--- scripts/main.js
+++ scripts/main.js
@@ -11,4 +11,16 @@ window.onscroll = function showHeader() {
     
 };
 
- 
+let anchors = document.querySelectorAll('header a[href*="#"]');
+
+for (anchor of anchors) {
+  if (anchor) {
+    anchor.addEventListener('click', function(e){
+      e.preventDefault();
+      anchorId = this.getAttribute('href');
+      document.querySelector(anchorId).scrollIntoView({
+        behavior: 'smooth', block: 'start'
+      })
+    })
+  }
+}; 
